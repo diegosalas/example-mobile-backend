fdef8ff7a55368b2fb72dff7da0b446ab6f74aa6
Diego Perez Salas <diegosalas@gmail.com>
2020-05-10 21:33:04 +0200

    test

--- web.rb
+++ web.rb
@@ -19,7 +19,7 @@ end
 
 get '/' do
   status 200
-  return log_info("Great, your backend is set up. Now you can configure the Stripe example apps to point here.")
+  return log_info("Great, your backend is set up. Now you can configure the Stripe example apps to point here. Alejandro")
 end
 
 post '/ephemeral_keys' do
